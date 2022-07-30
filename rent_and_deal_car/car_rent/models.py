3f052221c43f5717baaa5fa47fdbaf28f487dcac
TomekB <62642991+TomekBr@users.noreply.github.com>
2022-07-30 16:54:22 +0200

    creating 'Branch' in models

--- rent_and_deal_car/car_rent/models.py
+++ rent_and_deal_car/car_rent/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Branch(models.Model):
+    address = models.CharField(max_length=39)
+    city = models.CharField(max_length=16)
+    mobile = models.SmallIntegerField("+48", max_length=11)
+    opening_hours = models.TextField()
+    mail = models.EmailField("e-mail address", max_length=39)
+    remarks = models.TextField(null=True)
+
+    def __str__(self):
+        return f"Address {self.address} in {self.city} "
