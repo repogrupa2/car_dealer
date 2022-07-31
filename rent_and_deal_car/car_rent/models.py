abc522f61732fd3c06696587efb5e1d6a8bb3945
Pawel Bakowski <pwlbkw@yahoo.pl>
2022-07-31 10:33:42 +0200

    Adding Offer to models file

--- rent_and_deal_car/car_rent/models.py
+++ rent_and_deal_car/car_rent/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class RentalOffer(models.Model):
+    Vehicle_Id = models.ForeignKey(Vehicle)
+    Branch_Id_Availability = models.ForeignKey(Branch)
+    Availability = models.CharField(null=True, max_length=4)
+    Categories = models.CharField(max_length=16)
+    Description = models.TextField(null=True)
+    Deposit = models.DecimalField(2)
+    Price = models.DecimalField(2)
+
+    def __str__(self):
+        return f"Vehicle_Id: {self.Vehicle_Id}, {self.Branch_Id_Availability}, {self.Availability}, {self.Categories}, {self.Description}, {self.Deposit}, {self.Price}"
+
