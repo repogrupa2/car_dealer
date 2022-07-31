9fe8682144bbe39fb04900d2e6e96220a7bb7fc8
TomekB <62642991+TomekBr@users.noreply.github.com>
2022-07-31 12:12:57 +0200

    updated branch in models file

--- rent_and_deal_car/car_rent/models.py
+++ rent_and_deal_car/car_rent/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-# Create your models here.
 class Branch(models.Model):
     address = models.CharField(max_length=39)
     city = models.CharField(max_length=16)
-    mobile = models.SmallIntegerField("+48", max_length=11)
+    mobile = models.CharField(max_length=15)
     opening_hours = models.TextField()
-    mail = models.EmailField("e-mail address", max_length=39)
+    mail = models.EmailField(max_length=39)
     remarks = models.TextField(null=True)
 
     def __str__(self):
-        return f"Address {self.address} in {self.city} "
+        return f"Address {self.address} in {self.city}, opening hours {self.opening_hours}, mobile {self.mobile}, " \
+               f"mail {self.mail} "
