97f7fe0b4ae8b9107bef8eaef8d51ca31fbaec65
Piotr <nauka@o2.pl>
2022-08-04 21:02:40 +0200

    add comma

--- rent_and_deal_car/rent_and_deal_car/urls.py
+++ rent_and_deal_car/rent_and_deal_car/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("car_rent.urls"))
+    path('', include("car_rent.urls")),
     path('accounts/', include('accounts.urls')),
    ]
