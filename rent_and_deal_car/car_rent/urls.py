6a864c115be379a2ffa9553a33e84f80efeca34c
TomekB <62642991+TomekBr@users.noreply.github.com>
2022-08-04 20:38:57 +0200

    Making CRUD for Branch Table

---
+++ rent_and_deal_car/car_rent/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from .views import list_of_branch, get_branch, create_branch, edit_branch, delete_branch
+
+app_name = "car_rent"
+
+urlpatterns = [
+    path('branches/', list_of_branch,name = "list-of-branch"),
+    path('branch/<int:branch_id>', get_branch, name = "branch"),
+    path('create_branch/', create_branch,name = "create-branch"),
+    path('edit/<int:branch_id>', edit_branch,name = "edit-branch"),
+    path('delete/<int:branch_id>', delete_branch,name = "delete-branch"),
+    ]
--- rent_and_deal_car/car_rent/views.py
+++ rent_and_deal_car/car_rent/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import BranchCreate
+from .models import Branch
+
+
+def list_of_branch(request):
+    list_of_branches = Branch.objects.all()
+    return render(request, 'car_rent/list_of_branches.html', {'list_of_branches': list_of_branches})
+
+
+def create_branch(request):
+    form = BranchCreate()
+    if request.method == 'POST':
+        form = BranchCreate(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('car_rent:list-of-branch')
+
+    ctx={'form':form}
+    return render(request, 'car_rent/create_branch.html', ctx)
+
+
+def edit_branch(request, branch_id):
+    branch = Branch.objects.get(id=branch_id)
+    form = BranchCreate(instance=branch)
+    if request.method == 'POST':
+        form = BranchCreate(request.POST,instance=branch)
+        if form.is_valid():
+            form.save()
+            return redirect('car_rent:list-of-branch')
+
+    ctx = {'form': form}
+    return render(request, 'car_rent/create_branch.html',ctx)
+
+def delete_branch(request, branch_id):
+    branch = Branch.objects.get(id=branch_id)
+    if request.method == 'POST':
+        branch.delete()
+        return redirect('car_rent:list-of-branch')
+
+    ctx={'item':branch}
+    return render(request, 'car_rent/delete_branch.html', ctx)
+
+
+def get_branch(request, branch_id):
+    try:
+        branch = Branch.objects.get(id=branch_id)
+    except Branch.DoesNotExist:
+        ctx = {'branch': branch}
+
+    ctx = {'branch': branch}
+    return render(request, "car_rent/branch.html", context=ctx)
