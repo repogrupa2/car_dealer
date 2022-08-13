f37bb80811ff8dc85662adf62829b51c3701394e
Sebastian <zibisebek2193@gmail.com>
2022-08-13 13:37:28 +0200

    phoneregex

--- rent_and_deal_car/car_rent/urls.py
+++ rent_and_deal_car/car_rent/urls.py
@@ -11,39 +11,35 @@ app_name = "car_rent"
 
 urlpatterns = [
 
-    path('branches/', ListOfBranches.as_view(),name = "list-of-branch"),
-    path('branch/<int:branch_id>', ViewBranch.as_view(), name = "branch"),
-    path('create_branch/',CreateBranch.as_view(),name = "create-branch"),
-    path('edit_branch/<int:branch_id>', EditBranch.as_view(),name = "edit-branch"),
-    path('delete_branch/<int:branch_id>', BranchDelete.as_view(),name = "delete-branch"),
-    
-    path('vehiclelist/', VehicleList.as_view(), name="vehicle-list"),
-    path('addvehicle/', AddVehicle.as_view(), name="add-vehicle"),
-    path('updatevehicle/<int:vehicle_id>', UpdateVehicle.as_view(), name="update-vehicle"),
-    path('deletevehicle/<int:vehicle_id>', VehicleDelete.as_view(), name="delete-vehicle"),
+                  path('branches/', ListOfBranches.as_view(), name="list-of-branch"),
+                  path('branch/<int:branch_id>', ViewBranch.as_view(), name="branch"),
+                  path('create_branch/', CreateBranch.as_view(), name="create-branch"),
+                  path('edit_branch/<int:branch_id>', EditBranch.as_view(), name="edit-branch"),
+                  path('delete_branch/<int:branch_id>', BranchDelete.as_view(), name="delete-branch"),
 
-    path('listbrand/', BrandList.as_view(), name="list-brand"),
-    path('createbrand/', CreateBrand.as_view(), name="create-brand"),
+                  path('vehiclelist/', VehicleList.as_view(), name="vehicle-list"),
+                  path('addvehicle/', AddVehicle.as_view(), name="add-vehicle"),
+                  path('updatevehicle/<int:vehicle_id>', UpdateVehicle.as_view(), name="update-vehicle"),
+                  path('deletevehicle/<int:vehicle_id>', VehicleDelete.as_view(), name="delete-vehicle"),
 
-    path('listmodel/', ModelList.as_view(), name="list-model"),
-    path('createmodel/', CreateModel.as_view(), name="create-model"),
-    
-    path('list_of_offers/', ListOfRentalOffers.as_view(), name = "list-of-rental-offers"),
-    path('offer/<int:RentalOffer_id>', RentalOfferView.as_view, name="rental-offer"),
-    path('create_offer/', CreateOffer.as_view(), name = "upload-offer"),
-    path('edit/<int:RentalOffer_id>', RentalOfferEdit.as_view,name = "edit-offer"),
-    path('delete/<int:RentalOffer_id>', RentalOfferDelete.as_view,name = "delete-offer"),
+                  path('listbrand/', BrandList.as_view(), name="list-brand"),
+                  path('createbrand/', CreateBrand.as_view(), name="create-brand"),
 
-    path('car_rental/<int:id>',CarRentalDetails.as_view(), name="car_rental"),
+                  path('listmodel/', ModelList.as_view(), name="list-model"),
+                  path('createmodel/', CreateModel.as_view(), name="create-model"),
 
-    path('home/',home, name="home"),
-    path('aboutus/',aboutus, name="aboutus"),
-    path('account_details/',account_details, name="account-details"),
+                  path('list_of_offers/', ListOfRentalOffers.as_view(), name="list-of-rental-offers"),
+                  path('offer/<int:RentalOffer_id>', RentalOfferView.as_view, name="rental-offer"),
+                  path('create_offer/', CreateOffer.as_view(), name="upload-offer"),
+                  path('edit/<int:RentalOffer_id>', RentalOfferEdit.as_view, name="edit-offer"),
+                  path('delete/<int:RentalOffer_id>', RentalOfferDelete.as_view, name="delete-offer"),
 
-    path('customer/',CustomerAdd.as_view(), name="customer-add"),
-  
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('car_rental/<int:id>', CarRentalDetails.as_view(), name="car_rental"),
 
+                  path('home/', home, name="home"),
+                  path('aboutus/', aboutus, name="aboutus"),
+                  path('account_details/', account_details, name="account-details"),
 
+                  path('customer/', CustomerAdd.as_view(), name="customer-add"),
 
-
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
